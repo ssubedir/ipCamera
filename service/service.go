ae1147f66932d8ebf17b54d3cca166b8db6949f3
Rajan <ssubedir@gmail.com>
2020-05-16 14:28:38 -0400

    grpc client side streaming

--- service/service.go
+++ service/service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
+	"io"
 	"log"
 	"os"
 
@@ -33,6 +34,58 @@ func (s *Service) StreamImage(ctx context.Context, req *proto.StreamImageRequest
 	return &proto.StreamImageResponse{Response: "We got your picture"}, nil
 }
 
+func (s *Service) StreamVideo(stream proto.IpCamera_StreamVideoServer) error {
+
+	_, err := stream.Recv()
+
+	if err != nil {
+		log.Fatal("Error fetching stream")
+	}
+
+	videoData := bytes.Buffer{}
+
+	for {
+		err := contextError(stream.Context())
+		if err != nil {
+			return err
+		}
+
+		log.Print("waiting to receive more data")
+
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Print("no more data")
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("cannot receive chunk data: %v", err)
+		}
+
+		chunk := req.GetVideo()
+
+		// process chunk
+
+		_, err = videoData.Write(chunk)
+		if err != nil {
+			return fmt.Errorf("cannot write chunk data: %v", err)
+		}
+	}
+
+	res := &proto.StreamVideoResponse{
+		Response: "Finish Streaming.",
+	}
+
+	err = stream.SendAndClose(res)
+	if err != nil {
+		return fmt.Errorf("cannot send response: %v", err)
+	}
+
+	fmt.Println("Done Streaming")
+
+	return nil
+
+}
+
 func serveFrames(imgByte []byte) {
 
 	img, _, err := image.Decode(bytes.NewReader(imgByte))
@@ -51,4 +104,15 @@ func serveFrames(imgByte []byte) {
 		log.Println(err)
 	}
 
+}
+
+func contextError(ctx context.Context) error {
+	switch ctx.Err() {
+	case context.Canceled:
+		return fmt.Errorf("canceled")
+	case context.DeadlineExceeded:
+		return fmt.Errorf("DeadlineExceeded")
+	default:
+		return nil
+	}
 }
